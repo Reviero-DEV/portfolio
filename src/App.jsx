4b2ce42e88e904be7b5e0123c12534d2f2ea1b36
Reviero-DEV <netoreviero@gmail.com>
2026-03-14 23:49:05 -0300

    feat: adiciona mesa ao quarto 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import Room from "./scene/Room"
+import Desk from "./scene/Desk"
+
 export default function App() {
   return (
     <Canvas camera={{ position: [5, 3, 5], fov: 60 }}>
      
-      {/* luz ambiente */}
       <ambientLight intensity={ 0.6 } />
-      {/* luz principal */}
       <directionalLight position={[5, 10, 5]} intensity={1} />
       <Room />
+      <Desk />
       <OrbitControls />
      
     </Canvas>
---
+++ src/scene/Desk.jsx
@@ -0,0 +1,29 @@
+export default function Desk() {
+  return (
+    <>
+      <mesh position={[0, 1, -2]}>
+        <boxGeometry args={[3, 0.15 , 1.5 ]} />
+        <meshStandardMaterial color="#6b4f3b" />
+      </mesh>
+
+      <mesh position={[- 1.3 , 0.5 , - 2.6 ]}>
+        <boxGeometry args={[ 0.15 , 1, 0.15 ]} />
+        <meshStandardMaterial color="#3e2c20" />
+      </mesh>
+
+      <mesh position={[ 1.3 , 0.5 , - 2.6 ]}>
+        <boxGeometry args={[ 0.15 , 1, 0.15 ]} />
+        <meshStandardMaterial color="#3e2c20" />
+      </mesh>
+
+      <mesh position={[- 1.3 , 0.5 , - 1.4 ]}>
+        <boxGeometry args={[ 0.15 , 1, 0.15 ]} /> <
+        meshStandardMaterial color="#3e2c20" />
+      </mesh>
+
+      <mesh position={[ 1.3 , 0.5 , - 1.4 ]}>
+        <boxGeometry args={[ 0.15 , 1, 0.15 ]} />
+        <meshStandardMaterial color="#3e2c20" />
+      </mesh>
+    </>
+  ) }
