0aad9fcd8b724a73aab40ef8f51c09c043e35a6b
Reviero-DEV <netoreviero@gmail.com>
2026-03-16 23:30:48 -0300

    refactor(window): reposiciona janela para outra parede

--- src/scene/window.jsx
+++ src/scene/window.jsx
@@ -1,11 +1,11 @@
 export default function Window() {
     return (
         <>
-        <mesh position={[2.5, 2.5, -4.9]}>
+        <mesh position={[-4.9, 2.5, -2.5]} rotation={[0, Math.PI / 2, 0]}>
             <boxGeometry args={[2, 1.5, 0.1]} />
             <meshStandardMaterial color= "#cfcfcf" />
         </mesh>
-        <mesh position={[2.5, 2.5, -4.9]}>
+        <mesh position={[-4.9, 2.5, -2.5]} rotation={[0, Math.PI / 2, 0]}>
             <boxGeometry args={[2, 1.5, 0.1]} />
             <meshStandardMaterial color= "#87ceeb"
             transparent
