8c40a6d694cf915e2ebf313eb7d0a08c134ac38c
Reviero-DEV <netoreviero@gmail.com>
2026-03-16 23:16:10 -0300

    feat: adiciona acessorios da mesa

--- src/scene/Desk.jsx
+++ src/scene/Desk.jsx
@@ -1,29 +1,33 @@
+import Mousepad from "./Mousepad";
+
 export default function Desk() {
   return (
     <>
       <mesh position={[0, 1, -2]}>
-        <boxGeometry args={[3, 0.15 , 1.5 ]} />
+        <boxGeometry args={[3, 0.15, 1.5]} />
         <meshStandardMaterial color="#6b4f3b" />
+        <Mousepad />
       </mesh>
 
-      <mesh position={[- 1.3 , 0.5 , - 2.6 ]}>
-        <boxGeometry args={[ 0.15 , 1, 0.15 ]} />
+      <mesh position={[-1.3, 0.5, -2.6]}>
+        <boxGeometry args={[0.15, 1, 0.15]} />
         <meshStandardMaterial color="#3e2c20" />
       </mesh>
 
-      <mesh position={[ 1.3 , 0.5 , - 2.6 ]}>
-        <boxGeometry args={[ 0.15 , 1, 0.15 ]} />
+      <mesh position={[1.3, 0.5, -2.6]}>
+        <boxGeometry args={[0.15, 1, 0.15]} />
         <meshStandardMaterial color="#3e2c20" />
       </mesh>
 
-      <mesh position={[- 1.3 , 0.5 , - 1.4 ]}>
-        <boxGeometry args={[ 0.15 , 1, 0.15 ]} /> <
-        meshStandardMaterial color="#3e2c20" />
+      <mesh position={[-1.3, 0.5, -1.4]}>
+        <boxGeometry args={[0.15, 1, 0.15]} />{" "}
+        <meshStandardMaterial color="#3e2c20" />
       </mesh>
 
-      <mesh position={[ 1.3 , 0.5 , - 1.4 ]}>
-        <boxGeometry args={[ 0.15 , 1, 0.15 ]} />
+      <mesh position={[1.3, 0.5, -1.4]}>
+        <boxGeometry args={[0.15, 1, 0.15]} />
         <meshStandardMaterial color="#3e2c20" />
       </mesh>
     </>
-  ) }
+  );
+}
