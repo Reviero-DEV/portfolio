fcc9f8a749ba19a964cd775a8da94650fab3eb9f
Reviero-DEV <netoreviero@gmail.com>
2026-03-17 23:46:48 -0300

    feat: adiciona robo animado e carro de decoracao sobre a mesa

--- src/scene/Desk.jsx
+++ src/scene/Desk.jsx
@@ -1,13 +1,18 @@
 import Mousepad from "./Mousepad";
-
+import { Car } from "../components/car";
+import  Figure from "../components/figure";
 export default function Desk() {
   return (
     <>
-      <mesh position={[0, 1, -2]}>
-        <boxGeometry args={[3, 0.15, 1.5]} />
-        <meshStandardMaterial color="#6b4f3b" />
+      <group>
+        <mesh position={[0, 1, -2]}>
+          <boxGeometry args={[3, 0.15, 1.5]} />
+          <meshStandardMaterial color="#6b4f3b" />
+        </mesh>
         <Mousepad />
-      </mesh>
+        <Car position={[1.2, 1.08, -2]} scale={0.1} />
+        <Figure position={[1, 1, -2]} scale={0.1} />
+      </group>
 
       <mesh position={[-1.3, 0.5, -2.6]}>
         <boxGeometry args={[0.15, 1, 0.15]} />
