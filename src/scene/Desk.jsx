66cb110a26b58a574fdf01dd91f96a379587dfc4
Reviero-DEV <netoreviero@gmail.com>
2026-03-20 23:55:34 -0300

    style: ajusta posicionamento da mesa e ajusta objetos

--- src/scene/Desk.jsx
+++ src/scene/Desk.jsx
@@ -1,34 +1,44 @@
-import Mousepad from "./Mousepad";
-import { Car } from "../components/car";
-import Figure from "../components/figure";
+import Mousepad from "../components/desk/Mousepad";
+
+import Laptop from "../components/desk/Laptop";
+import CoffeeMug from "../components/desk/CoffeeMug";
+import DeskAccessories from "../components/desk/Acessories";
+import Monitor from "../components/desk/Monitor";
+
 export default function Desk() {
   return (
     <>
-      <group>
+      <group position={[-2, 0, -1.5]} scale={1.2}>
         <mesh position={[0, 1, -2]}>
-          <boxGeometry args={[3, 0.04, 1.5]} />
-          <meshStandardMaterial color="#8b5e3c" metalness={0.1} roughness={0.5} />
+          <boxGeometry args={[3.5, 0.04, 1.5]} />
+          <meshStandardMaterial
+            color="#8b5e3c"
+            metalness={0.1}
+            roughness={0.5}
+          />
           <Mousepad />
-          <Car position={[-0.7, 0.03, 0]} scale={0.1} rotation={[0, 0.5, 0]} />
-          <Figure position={[0.7, 0, 0.4]} scale={0.1} />
+          <CoffeeMug />
+          <Laptop />
+          <DeskAccessories />
+          <Monitor />
         </mesh>
 
-        <mesh position={[-1.3, 0.5, -2.6]}>
+        <mesh position={[-1.57, 0.5, -2.6]}>
           <boxGeometry args={[0.05, 1, 0.1]} />
           <meshStandardMaterial color="#444" roughness={1} metalness={1} />
         </mesh>
 
-        <mesh position={[1.3, 0.5, -2.6]}>
+        <mesh position={[1.57, 0.5, -2.6]}>
           <boxGeometry args={[0.05, 1, 0.1]} />
           <meshStandardMaterial color="#444" roughness={1} metalness={1} />
         </mesh>
 
-        <mesh position={[-1.3, 0.5, -1.4]}>
+        <mesh position={[-1.57, 0.5, -1.4]}>
           <boxGeometry args={[0.05, 1, 0.1]} />{" "}
           <meshStandardMaterial color="#444" roughness={1} metalness={1} />
         </mesh>
 
-        <mesh position={[1.3, 0.5, -1.4]}>
+        <mesh position={[1.57, 0.5, -1.4]}>
           <boxGeometry args={[0.05, 1, 0.1]} />
           <meshStandardMaterial color="#444" roughness={1} metalness={1} />
         </mesh>
