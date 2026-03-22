b4890cd8315bff8d382d0314d78b0482f96b12f9
Reviero-DEV <netoreviero@gmail.com>
2026-03-21 23:59:33 -0300

    refactor(Room): reoganiza codigo e estrutura do projeto

--- src/scene/Room.jsx
+++ src/scene/Room.jsx
@@ -1,28 +1,32 @@
+import { NamePlate } from "../components/decoration/NamePlate";
+import Rug from "../components/decoration/rug";
+import Desk from "../scene/Desk";
+import Bookshelf from "../scene/Bookshelf";
+import Chair from "../scene/Chair";
+import Wall from "./wall";
+
 export default function Room() {
   return (
     <>
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
         <planeGeometry args={[10, 10]} />
-        <meshStandardMaterial color="#8f8f8f" />
+        <meshStandardMaterial color="#cfcfcf" />
+        <Rug />
       </mesh>
+      <Desk />
+      <Chair />
 
       <mesh position={[0, 2.5, -5]} receiveShadow>
         <boxGeometry args={[10, 10, 0.2]} />
         <meshStandardMaterial color="#e5e5e5" />
         <mesh position={[0, -2.45, 0.11]} rotation={[0, Math.PI / 2, 0]}>
           <boxGeometry args={[0.02, 0.1, 10]} />
-          <meshStandardMaterial color="#0a0f52" />
+          <meshStandardMaterial color="#074572" />
         </mesh>
+        <Bookshelf />
+        <NamePlate />
       </mesh>
-      
-      <mesh position={[-5, 2.5, 0]}>
-        <mesh position={[0.1, -2.45, 0]}>
-          <boxGeometry args={[0.02, 0.1, 10]} />
-          <meshStandardMaterial color="#0a0f52" />
-        </mesh>
-        <boxGeometry args={[0.2, 5, 10]} />
-        <meshStandardMaterial color="#dddddd" />
-      </mesh>
+      <Wall/>
     </>
   );
 }
